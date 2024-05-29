61134d7a336f9b621938d03fdd47875249d2c698
ArthurLVidal <arthurodri2017@gmail.com>
2024-05-29 18:06:08 -0300

    Adicionando mais routes expecificamente a route dos detalhes de cada receita. além disso foi adicionado todos os detalhes das receitas no aplicativo, com o arquivo meals_detail_screen.dart, onde é apresentado cada item através dos seus indices

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/screens/meal_detail_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './utils/app_routes.dart';
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: Colors.amber,
           secondary: Colors.pink,
+          tertiary: Colors.amber,
         ),
         fontFamily: 'Raleway',
         canvasColor: const Color.fromRGBO(255, 254, 229, 1),
@@ -31,6 +33,28 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else if (settings.name == '/outra-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(
+      //       builder: (_) {
+      //         return CategoriesScreen();
+      //       },
+      //     );
+      //   }
+      // },
+      // P
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (_) {
+            return CategoriesScreen();
+          },
+        );
       },
     );
   }
